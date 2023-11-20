5ddafb55670e8b5048434ade90b40fa6137b67df
Yikoutian1 <qiulihang1@foxmail.com>
2023-11-20 21:29:10 +0800

    feat(SpringCloud Gateway):  网关配置与使用(simple use)

---
+++ hm-gateway/src/main/java/com/hang/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.hang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName GatewayApplication
+ * @Description Gateway
+ * @Author QiuLiHang
+ * @DATE 2023/11/20 020 21:15
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
